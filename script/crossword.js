371377264a204817df40cb8c731cd3e2ab4c9b0f
Preston Chapman <pchapman82070@uat.edu>
2024-06-27 10:38:35 -0700

    Add word to table

--- script/crossword.js
+++ script/crossword.js
@@ -5,9 +5,19 @@ function genTable(rowLen, colLen){
     for(let i=0; i<rowLen; i++){
         let row = document.createElement("tr")
             for(let j=0; j<colLen; j++){
-                let col = document.createElement('col')
+                let col = document.createElement('td')
                 row.appendChild(col)
             }
         TABLE.appendChild(row)
     }
+}
+
+function addWord(word, row, col, vert){
+    const TABLE = document.getElementById("crossword")
+    let array = word.split('')
+    console.log(array)
+    array.forEach((letter, i) => {
+        if(vert) TABLE.rows[row+i].cells[col].innerHTML = letter;
+        else TABLE.rows[row].cells[i+col].innerHTML = letter;
+    });
 }
