04d986170a2ecddc6dfa5eb25078f24aae95c1e2
Preston Chapman <pchapman82070@uat.edu>
2024-06-28 11:38:31 -0700

    add JSDOC

--- script/crossword.js
+++ script/crossword.js
@@ -1,19 +1,40 @@
 document.addEventListener("DOMContentLoaded", () => genTable(10,10))
-
 class Word{
+    /**
+     * @type {Array<String>}
+     */
     reaminingLetters = [];
     solved = false;
+    /**
+     * Generate a new word for the cross word puzzle
+     * @param {String} word - The word to be added to the crossword
+     * @param {Number} col - The starting collumn for the word (left=0) 
+     * - MUST be positive whole number
+     * @param {Number} row - The starting row for the word (top=0)
+     * - MUST be positive whole number
+     * @param {Boolean} vertical - Whether the word is vertical or horizontal 
+     * @param {String} hint - The hint that will be provided
+     */
     constructor(word, col, row, vertical, hint){
         this.word = word;
         this.col = col;
         this.row = row;
         this.vertical = vertical;
+        /** 
+         * @type {Array<String>}
+        */
         this.letters = this.word.split('')
+        /**
+         * @type {Array<String>}
+         */
         this.reaminingLetters = this.letters.slice(0);
         this.hint = hint;
     }
+    /**
+     * Add the word to the table
+     * @param {HTMLTableElement} TABLE - HTML Table element to add the word to
+     */
     addWord(TABLE){
-       
         this.letters.forEach((letter, i) => {
             let td=this.getCell(TABLE,i)
             // td.innerHTML = letter.toUpperCase;
@@ -24,6 +45,10 @@ class Word{
             td.classList.remove("unused");
         });
     }
+    /**
+     * Check if the word is correct
+     * @param {HTMLTableElement} TABLE - HTML Table element to check if the word is correct
+     */
     checkWord(TABLE){
         this.letters.forEach((letter, i) => {
             let td=this.getCell(TABLE, i);
@@ -35,6 +60,14 @@ class Word{
            
         })
     }
+    /**
+     * Check if the value (letter) is correct, handles no value inputed
+     * @param {HTMLTableColElement} td - Table Column for the letter to check
+     * @param {String} letter The correct letter
+     * @returns {boolean} 
+     * - True: The value is correct
+     * - False: The value is incorrect
+     */
     checkValue(td, letter){
         console.log(td.firstChild.value)
         if(td.firstChild.value == undefined) {
@@ -43,6 +76,10 @@ class Word{
         }
         return td.firstChild.value.toUpperCase() == letter.toUpperCase()
     }
+    /**
+     * Show the word in the table, while checking if it is correct or not
+     * @param {HTMLTableElement} TABLE - HTML Table element to show the word
+     */
     showWord(TABLE){
         console.log("Start Show Word")
         console.log(this.letters)
@@ -59,12 +96,28 @@ class Word{
             td.innerHTML = letter.toUpperCase();
         })
     }
+    /**
+     * 
+     * @param {HTMLTableElement} TABLE - HTML table to get the cell from
+     * @param {Number} i - Index to be added to the row/column
+     * - this.vertical: true | row + i
+     * - this.vertical: false | col + i
+     * @returns {HTMLTableColElement} The table column of the 
+     */
     getCell(TABLE, i){
         let td;
         if(this.vertical) td = TABLE.rows[this.row+i].cells[this.col];
         else td = TABLE.rows[this.row].cells[i+this.col];
         return td;
     }
+    /**
+     * Will continue to attempt to show letters until one is shown.
+     * @param {HTMLTableElement} TABLE - HTML table element to display the random letter
+     * @returns {boolean} 
+     * - False: No more letters left
+     * - True: Sucsesfully showed letter
+     */
+    
     showRandomLetter(TABLE){
         let randomIndex = Math.floor(Math.random() * this.letters.length);
         let td = this.getCell(TABLE, randomIndex);
@@ -90,6 +143,11 @@ class Word{
             return true;
         }
     }
+    /**
+     * Using the cell and index it will update the remaning letters and solved value
+     * @param {HTMLTableColElement} td 
+     * @param {Number} index - Index of the letter to update 
+     */
     updateSolved(td, index){
         td.innerHTML = this.letters[index].toUpperCase();
         this.reaminingLetters[index] = null;
@@ -98,11 +156,27 @@ class Word{
             this.solved = true;
         }
     }
+    /**
+     * Using the index check if that letter has been found
+     * @param {number} index - Index of the letter relative to the letters
+     * @returns {boolean}
+     * - True: Values Match
+     * - False: Values do not match
+     */
     forThisWord(index) {
         return this.reaminingLetters[index] == this.letters[index]
     }
 }
-
+/**
+ * Generate the table based on the collumn and row length
+ * The table will be colLen * rowlen in size
+ * Add Event Listeners to the buttons for the table
+ * Calls the addWords function
+ * @param {Number} colLen Total ammount of collumns
+ * - MUST be positive whole number 
+ * @param {Number} rowLen Total ammount of rows
+ * - MUST be positive whole number
+ */
 function genTable(colLen, rowLen){
     const TABLE = document.getElementById("crossword")
     for(let i=0; i<rowLen; i++){
@@ -126,17 +200,29 @@ const WORDS =[
     new Word("Cephalopod", 0,5,false, "The class of mollusks that includes squids, octopuses, and cuttlefish."),
     new Word("Mantle", 4,4,true, "The main body part of a squid, covering its organs.")
 ]
+
+/**
+ * Adds the words to the table
+ * For each word it will call the addWord method of the object
+ */
 function addWords(){
-    let hints = document.getElementById("hints")
     WORDS.forEach((word) => {
         word.addWord(document.getElementById("crossword"))
     })
 }
+/**
+ * Checks the words to see if they are correct
+ * for each word it will call the checkWord method of the object
+ */
 function checkWords(){
     WORDS.forEach((word) => {
         word.checkWord(document.getElementById("crossword"));
     })
 }
+/**
+ * Show all words with the correct values
+ * For each word it will call the showWord method of the object
+ */
 function showWords(){
     WORDS.forEach((word) => {
         word.showWord(document.getElementById("crossword"));
@@ -144,6 +230,12 @@ function showWords(){
 }
 var usedHints = []
 var secondHints = []
+/**
+ * Display a random hint
+ * Uses the hints from the words
+ * Once a hint is used it is added to usedHints
+ * If all hints are used it will generate hints based on the lenght of the word
+ */
 function randomHint(){
     let hints = document.getElementById("hints")
     let randomIndex = Math.floor(Math.random()*WORDS.length)
@@ -164,13 +256,15 @@ function randomHint(){
         randomWord = (randomIndex+1)+". "+WORDS[randomIndex].hint;
         usedHints.push(randomIndex)
     }
-    
-   
-    
     let hint = document.createElement("p");
     hint.innerHTML = randomWord;
     hints.appendChild(hint)
 }
+/**
+ * Display a random letter
+ * This function will call itself until there is a letter to display
+ * If there are no letters to display it will return
+ */
 function randomLetter(){
     let filteredWords = WORDS.filter((word) => !word.solved)
     console.log(filteredWords)
